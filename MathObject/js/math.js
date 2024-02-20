706c211a1d06507ffeb24f3c7839e99aad564749
Bilgin CAN <136249767+blgncan@users.noreply.github.com>
2024-02-20 10:50:04 +0300

    Add files via upload

---
+++ MathObject/js/math.js
@@ -0,0 +1,15 @@
+//Math.floor(); ==Virgülden sonrasını atar.
+    let a=3.99;
+//Math.ceil()   ==>Yukarı yuvarlar
+    let b=3.45;
+//Math.round()  ==>En yakına yuvarlar
+    let c=3.55;
+//Math.max()    ==> Dizideki en büyük değeri döner
+//Math.min()    ==> Dizideki en küçük değeri döner  
+//Math.abs()    ==> Bir sayının mutlak değerini almak için kullanılır.
+//Math.sqrt()   ==> Bir sayının karekökünü almak için kullanılır.
+//Math.pow()    ==> Bir sayının üssünü almak için kullanılır.
+//Math.random() ==> 0 ile 1 arasında rastgele bir sayı üretir.
+    let sayi=5;
+
+console.log(Math.floor(Math.random()*100));
