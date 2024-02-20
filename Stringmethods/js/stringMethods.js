172fbc074da849595a33e5d4b89346c6caf56684
Bilgin CAN <136249767+blgncan@users.noreply.github.com>
2024-02-20 10:12:10 +0300

    Add files via upload

---
+++ Stringmethods/js/stringMethods.js
@@ -0,0 +1,34 @@
+let letter="I have been learning javascript for a long time";
+let dailyDate="19.02.2024";
+//charAt()
+    let charecter=letter.charAt(5);
+//concat
+    let cancat=letter.concat(" "+dailyDate);
+//indexOf()
+    let result=letter.indexOf("v");
+//lastIndexOf()
+    let lastIndex=letter.lastIndexOf("f");
+//toUpperCase()
+    let upper=letter.toUpperCase();
+//lowerCase()
+    let lower=letter.toLowerCase();
+//trim()
+    let trim=letter.trim();
+//slice()
+    let slice=letter.slice(7,11);
+//subString()
+    let sub=letter.substring(2,6);
+//replace()
+    let replace=letter.replace("been","BEEN");
+//split()
+    let dizi=letter.split(" ");
+//valueOf()
+    let value=letter.valueOf();
+//startsWith()
+    let starts=letter.startsWith("I");
+//endsWith()
+    let end=letter.endsWith("e");
+
+    
+console.log(letter);
+console.log(end);
